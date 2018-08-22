99d19fe96cbd1eb1b15d166ad6e7ce3f7962b1fb
ElDainosor <eldainosor@gmail.com>
2018-08-22 18:37:03 +0000

    surnia: Bootleggers bringup

--- bootleg.mk
+++ bootleg.mk
@@ -16,13 +16,12 @@
 $(call inherit-product, device/motorola/surnia/full_surnia.mk)
 
 # Boot animation
-TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := bootleg_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
